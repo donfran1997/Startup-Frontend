8528e5f3eea3b4ce4571de2ede578c8f32230e2e
Aruzal <Travis_b53@hotmail.com>
2017-12-13 21:32:50 +1100

    Basic Scaffold and Project Start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SidePanelComponent } from './side-panel/side-panel.component';
+import { ChannelComponent } from './channel/channel.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidePanelComponent,
+    ChannelComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/side-panel/side-panel.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-panel',
+  templateUrl: './side-panel.component.html',
+  styleUrls: ['./side-panel.component.css']
+})
+export class SidePanelComponent implements OnInit {
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
